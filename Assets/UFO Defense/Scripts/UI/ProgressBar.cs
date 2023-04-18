eb92c26753ee4459a36d49c4e03dc2a7c94d0c4c
Nikita <japskiddin@gmail.com>
2023-04-18 19:11:12 +0800

    * Updated progress bar, added thumb picture

--- Assets/UFO Defense/Scripts/UI/ProgressBar.cs
+++ Assets/UFO Defense/Scripts/UI/ProgressBar.cs
@@ -7,35 +7,80 @@ namespace UFO_Defense.Scripts.UI
     {
         [Header("UI Elements")]
         [SerializeField] private Image image;
+        [SerializeField] private Image thumb;
 
         [Header("Properties")]
         [SerializeField] private int value;
-
         [SerializeField] private int maxValue = 100;
         [SerializeField] private bool isCorrectConfigured;
+        [SerializeField] private bool hasThumb;
+
+        private const int OriginLeft = 0;
+        private const int OriginRight = 1;
 
         private void Awake()
         {
-            if (image.type == Image.Type.Filled && image.fillMethod == Image.FillMethod.Horizontal)
-            {
-                isCorrectConfigured = true;
-            }
-            else
+            var isImageFillConfigured = image.type == Image.Type.Filled && image.fillMethod == Image.FillMethod.Horizontal;
+            if (!isImageFillConfigured)
             {
                 if (Debug.isDebugBuild)
                 {
-                    Debug.Log(
+                    Debug.LogError(
                         "{GameLog} => [ProgressBar] - (<color=red>Error</color>) -> Components Parameters Are Incorrectly Configured! \n " +
                         "Required Type Filled \n" +
                         "Required FillMethod Horizontal");
                 }
+                return;
             }
+            if (hasThumb)
+            {
+                if (thumb == null)
+                {
+                    if (Debug.isDebugBuild)
+                    {
+                        Debug.LogError(
+                            "{GameLog} => [ProgressBar] - (<color=red>Error</color>) -> Components Parameters Are Incorrectly Configured! \n " +
+                            "Required Image Thumb");
+                    }
+                    return;
+                }
+
+                InitThumbPosition();
+            }
+
+            isCorrectConfigured = true;
         }
 
         private void LateUpdate()
         {
             if (!isCorrectConfigured) return;
             image.fillAmount = (float)value / maxValue;
+            UpdateThumbPosition();
+        }
+
+        private void InitThumbPosition()
+        {
+            var thumbRect = thumb.GetComponent<RectTransform>();
+            if (image.fillOrigin == OriginRight)
+            {
+                thumbRect.anchorMin = new Vector2(0, 0.5f);
+                thumbRect.anchorMax = new Vector2(0, 0.5f);
+            }
+            else
+            {
+                thumbRect.anchorMin = new Vector2(1, 0.5f);
+                thumbRect.anchorMax = new Vector2(1, 0.5f);
+            }
+            thumbRect.anchoredPosition = new Vector2(0, 0);
+        }
+
+        private void UpdateThumbPosition()
+        {
+            if (!isCorrectConfigured || !hasThumb) return;
+            var progressWidth = image.GetComponent<RectTransform>().rect.width;
+            var thumbX = progressWidth - progressWidth * image.fillAmount;
+            var thumbRect = thumb.GetComponent<RectTransform>();
+            thumbRect.anchoredPosition = new Vector2(thumbX * (image.fillOrigin == OriginLeft ? -1 : 1), 0);
         }
 
         public void SetValue(int newValue)
